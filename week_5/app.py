dd18f59b232a18eacdd032a592f3960c7083679f
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-24 23:10:38 +0900

    update

--- week_5/app.py
+++ week_5/app.py
@@ -13,33 +13,38 @@ db = client.dbsparta
 def home():
     return render_template("index.html")
 
-
-@app.route("/guestbook", methods=["POST"])
-def guestbook_post():
-    name_receive = request.form["name_give"]
-    comment_receive = request.form["comment_give"]
-
-    doc = {"name": name_receive, "comment": comment_receive}
-
-    db.fan.insert_one(doc)
-    return jsonify({"msg": "응원 완료!"})
-
-
+############## GET (READ) ################
 @app.route("/guestbook", methods=["GET"])
 def guestbook_get():
     comments = list(db.fan.find())
     comments = [{**comment, **{"_id": str(comment["_id"])}} for comment in comments] # convert ObjectId to string
     return jsonify({'result': comments})
 
+############## POST (CREATE) ################
+@app.route("/guestbook", methods=["POST"])
+def guestbook_post():
+    name_receive = request.form["name_give"]
+    comment_receive = request.form["comment_give"]
+    doc = {"name": name_receive, "comment": comment_receive}
+    db.fan.insert_one(doc)
+    return jsonify({"msg": "응원 완료!"})
 
-
-@app.route("/delete_updatebook", methods=["DELETE"])
+############## DELETE (DELETE) ################
+@app.route("/guestbook", methods=["DELETE"])
 def guestbook_delete():
-    name_receive = request.form["id"]
-    print(name_receive)
-    db.fan.delete_one({'_id': ObjectId(name_receive)})
+    id_receive = request.form["id"]
+    print(id_receive)
+    db.fan.delete_one({'_id': ObjectId(id_receive)})
     return jsonify({'result': "success"})
 
+############## PUT/PATCH (UPDATE) ################
+@app.route("/guestbook", methods=["PUT"])
+def guestbook_update():
+    id_receive = request.form["id"]
+    new_comment = request.form["new_comment"]
+    print(id_receive, new_comment)
+    db.fan.update_one({'_id': ObjectId(id_receive)}, {"$set": {"comment": new_comment}})
+    return jsonify({'result': "success"})
 
 
 if __name__ == '__main__':
